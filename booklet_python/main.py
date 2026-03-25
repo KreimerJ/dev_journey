5b4a6882dcc747f9fb8457132d0267aeacd109a3
kreimer <kranterprog@gmail.com>
2026-03-25 08:13:05 -0500

    feature: file reading function implemented

--- booklet_python/main.py
+++ booklet_python/main.py
@@ -1,1 +1,13 @@
-print('greetings boots')
+def get_book_text():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    lines = get_book_text()
+    print(lines)
+
+
+if __name__ == "__main__":
+    main()
