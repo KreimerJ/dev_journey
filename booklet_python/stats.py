0a2a7b24d30d3b73c952ada4b6e8faa6d5cfe84b
kreimer <kranterprog@gmail.com>
2026-03-27 16:31:13 -0500

    Refactor: count_chars(text) in stats.py less lines, better performance using .get() for mapping dictionary

--- booklet_python/stats.py
+++ booklet_python/stats.py
@@ -8,9 +8,6 @@ def count_chars(text: str) -> dict:
     text = text.lower()
     chars = {}
     for char in text:
-        if char not in chars:
-            chars[char] = 1
-        else:
-            chars[char] = chars[char] + 1
+        chars[char] = chars.get(char, 0) + 1
 
     return chars
