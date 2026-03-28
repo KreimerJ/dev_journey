705c768931d3df94d147bb8d2d14e17ad867279c
kreimer <kranterprog@gmail.com>
2026-03-27 19:06:46 -0500

    Implement: map_list-to_dict(), sort_on() show_ordered_chars_count_list() functions in stats.py and main py. completed logic for mapping a dict into a list of dictionaries and printing only the value of each key of the dictionary in the same line.

--- booklet_python/main.py
+++ booklet_python/main.py
@@ -1,4 +1,10 @@
-from stats import count_chars, count_words
+from stats import (
+    count_chars,
+    count_words,
+    map_list_to_dict,
+    show_ordered_chars_count_list,
+    sort_on,
+)
 
 
 def get_book_text():
@@ -8,10 +14,21 @@ def get_book_text():
 
 
 def main():
+    print("=" * 12 + " BOOKBOT " + "=" * 12)
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("-" * 12 + " Word Count " + "-" * 12)
+
     content = get_book_text()
     words_number = count_words(content)
     print(f"Found {words_number} total words")
-    print(count_chars(content))
+    print("-" * 12 + " Character Count" + "-" * 12)
+
+    char_dict = count_chars(content)
+    list_of_chars = map_list_to_dict(char_dict)
+    list_of_chars.sort(reverse=True, key=sort_on)
+    show_ordered_chars_count_list(list_of_chars)
+
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- booklet_python/stats.py
+++ booklet_python/stats.py
@@ -11,3 +11,24 @@ def count_chars(text: str) -> dict:
         chars[char] = chars.get(char, 0) + 1
 
     return chars
+
+
+def map_list_to_dict(dictionary: dict) -> list[dict]:
+    sorted_list = []
+
+    for key, value in dictionary.items():
+        sorted_list.append({"char": key, "num": value})
+
+    return sorted_list
+
+
+def sort_on(item: dict):
+    return item["num"]
+
+
+def show_ordered_chars_count_list(list_of_chars):
+    for i in range(len(list_of_chars)):
+        if not list_of_chars[i]["char"].isalpha():
+            pass
+        else:
+            print(f"{list_of_chars[i]['char']}: {list_of_chars[i]['num']}")
